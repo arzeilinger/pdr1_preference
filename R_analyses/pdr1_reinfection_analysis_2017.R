3e9fede466b1134f48bda58760ea474ce93dbd08
arzeilinger <arzeilinger@gmail.com>
2018-01-15 16:30:22 -0800

    Improved glmer() model by removing non-sense interactions, bobyqa optimization

--- R_analyses/pdr1_reinfection_analysis_2017.R
+++ R_analyses/pdr1_reinfection_analysis_2017.R
@@ -7,6 +7,7 @@ my.packages <- c("tidyr", "dplyr", "data.table", "openxlsx", "ggplot2",
 lapply(my.packages, require, character.only = TRUE)
 
 source("R_functions/factor2numeric.R")
+source("R_functions/standardize.R")
 
 #### Import culturing data
 # reinfdata <- read.xlsx("data/2017_data/PdR1_2017_preference-transmission_experiment_data.xlsx", sheet = "Re-infection_culturing_data")
@@ -27,38 +28,44 @@ summary(reinfdata)
 reinfdata[is.na(reinfdata$xf_cfu_per_g),]
 # NAs for xf_cfu_per_g are mostly controls and dead plants
 # For sample 9-007S-4, xf_cfu_d0 was too contaminated but d1 and d2 were 0 could be imputed, but I'll remove it
-# Remove NAs
-reinfdata <- reinfdata %>% dplyr::filter(!is.na(xf_cfu_per_g))
+# Remove NAs and controls
+reinfdata <- reinfdata %>% dplyr::filter(., trt != "CAB-") %>% dplyr::filter(!is.na(xf_cfu_per_g)) 
 # Remove samples cultured on 2017-07-21 because all were negative, something went wrong, not sure what
 reinfdata <- reinfdata %>% dplyr::filter(xf_plant_date_cultured != "2017-07-21")
 
 #### Label infection treatments according to reps
-reinfdata$inoc.trt <- factor(with(reinfdata, ifelse(rep <= 4, "inoc1",
+reinfdata$inoc.time <- factor(with(reinfdata, ifelse(rep <= 4, "inoc1",
                                                     ifelse(rep >= 9, "inoc2",
                                                            "inoc1-2"))))
 summary(reinfdata)
 
 #### Do I have positive culture from all of the plants?
-reinfdata <- reinfdata %>% dplyr::filter(., trt != "CAB-") %>% dplyr::filter(!is.na(xf_cfu_per_g)) 
 sumcfu <- reinfdata %>% group_by(genotype, rep) %>% summarise(sum = sum(xf_cfu_per_g, na.rm = TRUE))
 print.data.frame(sumcfu)
 
 # Check how many times plants were negative that were positive previously
-reinflist <- reinfdata %>% dplyr::select(week, genotype, rep, xf_cfu_per_g, inoc.trt) %>% data.table() %>% split(., by = c("rep", "genotype"))
+reinflist <- reinfdata %>% dplyr::select(week, genotype, rep, xf_cfu_per_g, inoc.time) %>% data.table() %>% split(., by = c("rep", "genotype"))
 reinflist
 # Hard to say when zeros were false negatives, can't justify altering them
 
 #### Analysis of re-infection data using Poisson GLMM
 # Random effect on plantID because of repeated measures -- is very hard to fit though -- need to re-scale
 reinfdata$plantID <- factor(with(reinfdata, paste(genotype, trt, rep, sep = "-")))
-reinfMod1 <- glmer(xf_cfu_per_g ~ week + genotype + inoc.trt + (1|plantID), data = reinfdata, family = "poisson")
+# Re-scale week variable
+reinfdata$std.week <- standardize(reinfdata$week)
+# Remove week == 5 points because I'm lacking data for 092 and 094
+reinfdata <- reinfdata %>% dplyr::filter(week != 5)
+
+reinfMod1 <- glmer(xf_cfu_per_g ~ week*genotype + inoc.time + inoc.time:genotype + (1|plantID), data = reinfdata, family = "poisson",
+                   control = glmerControl(optimizer = "bobyqa"))
+reinfMod2 <- glmer(xf_cfu_per_g ~ week*trt + inoc.time + inoc.time:trt + (1|plantID), data = reinfdata, family = "poisson",
+                   control = glmerControl(optimizer = "bobyqa"))
 plot(reinfMod1)
 summary(reinfMod1)
 
 #### Summarize and plot reinfection data
 reinfSummary <- reinfdata %>% mutate(xfpoplog = log10(xf_cfu_per_g + 1)) %>% 
-  dplyr::filter(week != 5) %>% # Remove week 5 because it's a mess
-  group_by(week, trt, inoc.trt) %>% 
+  group_by(week, trt, inoc.time) %>% 
   summarise(mean = mean(xfpoplog, na.rm = TRUE),
             n = sum(!is.na(xfpoplog)),
             se = sd(xfpoplog)/sqrt(n))
@@ -67,8 +74,8 @@ print.data.frame(reinfSummary)
 
 #### Plotting mean xf pops
 meanreinfplot <- ggplot(data=reinfSummary, aes(x=week, y=mean)) +
-  geom_line(aes(linetype=inoc.trt, colour = trt), size=1.25) +
-  geom_point(aes(shape=inoc.trt, colour = trt), size=3.5) +
+  geom_line(aes(linetype=inoc.time, colour = trt), size=1.25) +
+  geom_point(aes(shape=inoc.time, colour = trt), size=3.5) +
   geom_errorbar(aes(ymax=mean+se, ymin=mean-se), width=0.2) +
   scale_x_continuous(name = "Weeks post inoculation", 
                      breaks = unique(reinfSummary$week)) + 
@@ -88,6 +95,39 @@ meanreinfplot
 
 ggsave("results/figures/2017_figures/reinfection_line_plot_2017.jpg", plot = meanreinfplot,
        width = 7, height = 7, units = "in")
+
+
+#### Plotting genotypes separately using facetting
+reinfSummary2 <- reinfdata %>% mutate(xfpoplog = log10(xf_cfu_per_g + 1)) %>% 
+  group_by(week, genotype, trt, inoc.time) %>% 
+  summarise(mean = mean(xfpoplog, na.rm = TRUE),
+            n = sum(!is.na(xfpoplog)),
+            se = sd(xfpoplog)/sqrt(n))
+reinfSummary2$se[is.na(reinfSummary2$se)] <- 0 # 26wk-092S only has one observation; set SE = 0 since xf_cfu = 0
+print.data.frame(reinfSummary2)
+
+# Plot of reinfection dynamis by genotype and inoculation time (inoc.time)
+reinfplot <- ggplot(data=reinfSummary2, aes(x=week, y=mean)) +
+  geom_line(aes(linetype=inoc.time, colour = trt), size=1.25) +
+  geom_point(aes(shape=inoc.time, colour = trt), size=3.5) +
+  geom_errorbar(aes(ymax=mean+se, ymin=mean-se), width=0.2) +
+  facet_wrap(~genotype) +
+  scale_x_continuous(name = "Weeks post inoculation", 
+                     breaks = unique(reinfSummary2$week)) + 
+  scale_y_continuous(name = "Xylella populations in plant (log10)",
+                     limits = c(0,10)) +
+  theme_bw(base_size=18) +
+  theme(axis.line = element_line(colour = "black"),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_rect(colour = "black"),
+        panel.background = element_blank()) 
+
+reinfplot
+
+ggsave("results/figures/2017_figures/reinfection_genotype_line_plot_2017.jpg", plot = reinfplot,
+       width = 7, height = 7, units = "in")
+
 
 
 
@@ -111,3 +151,4 @@ reinfplantplot <- ggplot(data=reinfdata, aes(x = week, y = log10(xf_cfu_per_g+1)
 reinfplantplot
 # ggsave("results/figures/vector_xf_line_plot.jpg", plot = vectorxfplot,
 #        width = 7, height = 7, units = "in")
+
