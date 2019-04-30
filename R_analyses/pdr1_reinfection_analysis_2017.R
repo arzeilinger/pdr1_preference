b54ce58c9c51a5c6d2c1126cff2f1aa874d5866a
arzeilinger <arzeilinger@gmail.com>
2019-04-30 16:24:52 -0700

    Finalized analysis and figures for reinfection experiment.

--- R_analyses/pdr1_reinfection_analysis_2017.R
+++ R_analyses/pdr1_reinfection_analysis_2017.R
@@ -2,8 +2,8 @@
 
 rm(list = ls())
 # Load packages
-my.packages <- c("tidyr", "dplyr", "data.table", "openxlsx", "ggplot2",
-                 "MASS", "logistf", "multcomp", "bbmle", "lme4")
+my.packages <- c("tidyr", "dplyr", "data.table", "openxlsx", "ggplot2", "DHARMa",
+                 "MASS", "logistf", "multcomp", "bbmle", "lme4", "googlesheets", "broom")
 lapply(my.packages, require, character.only = TRUE)
 
 source("R_functions/factor2numeric.R")
@@ -15,6 +15,8 @@ source("R_functions/standardize.R")
 pdr1DataURL <- gs_url("https://docs.google.com/spreadsheets/d/14uJLfRL6mPrdf4qABeGeip5ZkryXmMKkan3mJHeK13k/edit?usp=sharing",
                       visibility = "private")
 reinfdataGS <- gs_read(pdr1DataURL, ws = "Re-infection_culturing_data")
+
+
 reinfdata <- reinfdataGS
 str(reinfdata)
 summary(reinfdata)
@@ -48,6 +50,10 @@ reinflist <- reinfdata %>% dplyr::select(week, genotype, rep, xf_cfu_per_g, inoc
 reinflist
 # Hard to say when zeros were false negatives, can't justify altering them
 
+
+
+########################################################################################################
+#### Analysis
 #### Analysis of re-infection data using Poisson GLMM
 # Random effect on plantID because of repeated measures -- is very hard to fit though -- need to re-scale
 reinfdata$plantID <- factor(with(reinfdata, paste(genotype, trt, rep, sep = "-")))
@@ -60,9 +66,13 @@ reinfMod1 <- glmer(xf_cfu_per_g ~ week*genotype + inoc.time + inoc.time:genotype
                    control = glmerControl(optimizer = "bobyqa"))
 reinfMod2 <- glmer(xf_cfu_per_g ~ week*trt + inoc.time + inoc.time:trt + (1|plantID), data = reinfdata, family = "poisson",
                    control = glmerControl(optimizer = "bobyqa"))
-plot(reinfMod1)
+plot(simulateResiduals(reinfMod1)) ### Not a good model
 summary(reinfMod1)
 
+
+
+########################################################################################################
+#### Plotting
 #### Summarize and plot reinfection data
 reinfSummary <- reinfdata %>% mutate(xfpoplog = log10(xf_cfu_per_g + 1)) %>% 
   group_by(week, trt, inoc.time) %>% 
@@ -125,7 +135,30 @@ reinfplot <- ggplot(data=reinfSummary2, aes(x=week, y=mean)) +
 
 reinfplot
 
-ggsave("results/figures/2017_figures/reinfection_genotype_line_plot_2017.jpg", plot = reinfplot,
+
+## BW plot for paper
+reinfplotBW <- ggplot(data=reinfSummary2, aes(x=week, y=mean)) +
+  geom_line(aes(linetype=inoc.time), colour = "black", size=1.25) +
+  geom_point(aes(shape=inoc.time), colour = "black", size=3) +
+  geom_errorbar(aes(ymax=mean+se, ymin=mean-se), width=0.2) +
+  facet_wrap(~genotype) +
+  scale_x_continuous(name = "Weeks post inoculation", 
+                     breaks = unique(reinfSummary2$week)) + 
+  scale_y_continuous(name = "Xylella population size (log10, CFU/mg)",
+                     limits = c(0,10)) +
+  scale_shape_manual(values = c(0,1,2)) +
+  labs(linetype = "Treatment", shape = "Treatment") +
+  theme_bw(base_size=14) +
+  theme(axis.line = element_line(colour = "black"),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_rect(colour = "black"),
+        panel.background = element_blank(),
+        strip.background = element_blank()) 
+
+reinfplotBW
+
+ggsave("results/figures/2017_figures/reinfection_genotype_line_plot_2017_BW.jpg", plot = reinfplotBW,
        width = 7, height = 7, units = "in")
 
 
@@ -133,11 +166,10 @@ ggsave("results/figures/2017_figures/reinfection_genotype_line_plot_2017.jpg", p
 
 #### Plotting the dyanmics of individual plants
 reinfplantplot <- ggplot(data=reinfdata, aes(x = week, y = log10(xf_cfu_per_g+1))) +
-  #geom_line(aes(colour = trt), size=1.25) +
-  geom_point(aes(shape = genotype, colour = trt), size=2.5) +
-  #geom_errorbar(aes(ymax=meancfu+secfu, ymin=meancfu-secfu), width=0.2) +
+  geom_point(aes(shape=inoc.time, colour = trt), size=3.5) +
+  facet_wrap(~genotype) +
   scale_x_continuous(name = "Weeks post inoculation", 
-                     breaks = c(2,5,9)) + 
+                     breaks = unique(reinfdata$week)) + 
   scale_y_continuous(name = "Xylella CFU per g plant tissue (log 10)") +
   # ylab("% insects on source plant") + 
   # ylim(c(0,100)) +
@@ -151,4 +183,74 @@ reinfplantplot <- ggplot(data=reinfdata, aes(x = week, y = log10(xf_cfu_per_g+1)
 reinfplantplot
 # ggsave("results/figures/vector_xf_line_plot.jpg", plot = vectorxfplot,
 #        width = 7, height = 7, units = "in")
+
+
+
+
+########################################################################################################
+########################################################################################################
+#### Analysis and plotting of just 2nd inoculation (weeks 21 and 26) using ANCOVA
+inoc2dat <- reinfdata %>% dplyr::filter(week >= 21 & inoc.time != "inoc1")
+summary(inoc2dat)
+
+
+## GLM Poisson and quasiPoisson don't work well
+## Try negative binomial
+inoc2NB <- glm.nb((xf_cfu_per_g+1) ~ week*trt*inoc.time, data = inoc2dat,
+                  control = glm.control(epsilon = 1e-16, maxit = 100))
+summary(inoc2NB)
+## Plotting options not available for glm.nb
+## Check constant error variance
+residNB <- resid(inoc2NB)
+predNB <- predict(inoc2NB)
+plot(x = predNB, y = residNB)
+## Not constant
+
+## Linear model
+boxcox((xf_cfu_per_g+1) ~ week*trt*inoc.time, data = inoc2dat, lambda = seq(-2, 2, by=0.5))
+inoc2mod <- lm(sqrt(xf_cfu_per_g+1) ~ week*trt*inoc.time, data = inoc2dat)
+plot(simulateResiduals(inoc2mod)) 
+summary(inoc2mod)
+
+## Get 95% CI
+tidy(inoc2NB, conf.int = TRUE)
+
+
+## RESULTS: Both NB and linear model return similar results: week main effect is significant, other terms are not significant
+
+
+#### Plotting
+inoc2Summary <- inoc2dat %>% mutate(xfpoplog = log10(xf_cfu_per_g + 1)) %>% 
+  group_by(week, trt, inoc.time) %>% 
+  summarise(mean = mean(xfpoplog, na.rm = TRUE),
+            n = sum(!is.na(xfpoplog)),
+            se = sd(xfpoplog)/sqrt(n))
+print.data.frame(inoc2Summary)
+
+
+#### Plotting mean xf pops
+meaninoc2plot <- ggplot(data=inoc2Summary, aes(x=week, y=mean)) +
+  ## inoc.time: inoc1-2 = open circles; inoc1 = open triangles
+  ## trt: R = solid line; S = dashed line
+  geom_line(aes(linetype=trt, colour = inoc.time), size=1.25) +
+  geom_point(aes(shape=inoc.time), colour = "black", size=3) +
+  geom_errorbar(aes(ymax=mean+se, ymin=mean-se), width=0.2) +
+  scale_x_continuous(name = "Weeks post inoculation", 
+                     breaks = unique(inoc2Summary$week)) + 
+  scale_y_continuous(name = "Xylella population size (log10, CFU/g)",
+                     limits = c(0,10)) +
+  scale_color_manual(values = c("black", "black")) + 
+  scale_shape_manual(values = c(1,2)) +
+  theme_bw(base_size=14) +
+  theme(axis.line = element_line(colour = "black"),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_rect(colour = "black"),
+        panel.background = element_blank(),
+        legend.position = "none")
+
+meaninoc2plot
+
+ggsave("results/figures/2017_figures/reinfection_inoc2_line_plot_2017.jpg", plot = meaninoc2plot,
+       width = 7, height = 7, units = "in")
 
